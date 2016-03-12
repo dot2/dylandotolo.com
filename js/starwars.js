864d51b72e6da3ff75b7dd5ff3345e022fcdad89
Dylan D <dotolo.dylan2@gmail.com>
2016-03-11 22:33:55 -0600

    added new home page with card, bb8 animation, and the start of social link cards

---
+++ js/starwars.js
@@ -0,0 +1,6 @@
+var bb8 = document.getElementById("bb8"),
+    tween;
+
+// TweenLite.fromTo($bb8, 4, {x: '+1000px', repeat: 10, yoyo:true, onRepeat:onRepeat}, {x: -1000, ease:Power4.SlowMo});
+
+tween = TweenMax.to(bb8, 4, {left: "-450px", delay: 3, repeat: 1, repeatDelay: 0.5,  yoyo:false, ease:Power0.SlowMo});
