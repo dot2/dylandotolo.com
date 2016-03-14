52a5809177dda33063d0f11d07a54e9b51cb01db
Dylan Dotolo <dotolo.dylan2@gmail.com>
2016-03-14 17:42:41 -0500

    added resume link from home page, added back arrow to return to index.html

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-$(document).foundation();
+var dotolo = angular.app('dotolo', []);
+
+//Routing for pages
+dotolo.config(function($routeProvider){
+    $routeProvider.when("/"),
+    {
+        templateUrl: 'pages/home.html'
+    };
+});
--- js/min/app-min.js
+++ js/min/app-min.js
@@ -1,2 +1,2 @@
-$(document).foundation();
+var dotolo=angular.app("dotolo",[]);dotolo.config(function(o){o.when("/"),{templateUrl:"pages/home.html"}});
 //# sourceMappingURL=./app-min.js.map
